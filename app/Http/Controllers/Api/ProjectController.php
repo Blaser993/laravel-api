535e7520198a28002d4457f636c69a7951e283c0
Blaser993 <corradodepinto.design@gmail.com>
2023-05-02 15:44:14 +0200

    FEAT Mileston 2 completed

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $project = Project::all();
+        $project = Project::limit(10)->get();
 
         return response()->json([
-            'projects' => $projects,
+            'projects' => $project
         ]);
     }
 }
